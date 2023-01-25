536dabf249be7a7c46a93eae0c52d10f0e7493ed
UlisesC12 <ulisescandela99@gmail.com>
2023-01-25 01:20:53 -0600

    add product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = ''; //https://www.w3schools.com/howto/img_avatar.png
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Juguete Saltarín',
+      price: 50,
+      image: './../assets/images/toy.png'
+    },
+    {
+        id: '2',
+        name: 'Jerry Peluche',
+        price: 80,
+        image: './../assets/images/jerry.png'
+    },
+    {
+        id: '3',
+        name: 'Bicicleta Maravilla',
+        price: 300,
+        image: './../assets/images/bike.png'
+    },
+    {
+      id: '4',
+      name: 'Libro Vivir de Las Fibras',
+      price: 45,
+      image: './../assets/images/libro.png'
+  }
+  ];
 
   onLoaded(img: string) {
     console.log('log padre, ', img);
